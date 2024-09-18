7c788164db12796f68fa870f49a39589e0adcd1c
FaiqRN <faiqramzy2004@gmail.com>
2024-09-18 12:24:51 +0700

    Jobsheet 4 pwl

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,37 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User</title>
-    <style>
-        .container {
-            text-align: center;
-            font-family: Arial, sans-serif;
-            margin-top: 50px;
-        }
-        table {
-            margin: 0 auto;
-            border: 1px solid black;
-            padding: 10px;
-        }
-        th, td {
-            padding: 10px;
-            border: 1px solid black;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
+<html>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
         <h1>Data User</h1>
-        <table>
+        <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
             </tr>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
+            @foreach ( $data as $d )
+                <tr>
+                    <td>{{$data->user_id}}</td>
+                    <td>{{$data->username}}</td>
+                    <td>{{$data->nama}}</td>
+                    <td>{{$data->level_id}}</td>
+                </tr>
+            @endforeach
         </table>
-    </div>
-</body>
+    </body>
 </html>
